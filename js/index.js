91c7addfcad04eced1f150184d821bb39178861c
Janybek02 <janybek02x@gmail.com>
2022-03-17 19:04:18 +0600

    eiorhf

---
+++ js/index.js
@@ -0,0 +1,77 @@
+$('.slider--slide--page--div').slick({
+    centerMode: true,
+    dots: true,
+    centerPadding: '150px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        }
+    ]
+});
+$('.slider2__slide').slick({
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 2.5,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '00px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '80px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+$('.slider--slide2--page--div').slick({
+    centerMode: true,
+    dots: true,
+    centerPadding: '150px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        }
+    ]
+});
